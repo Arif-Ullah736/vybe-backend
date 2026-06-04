93375c4cb1d621ea3c592d94e8dc1e3c63d75979
Arif Ullah <arifkhattak736@gmail.com>
2026-06-04 23:17:31 +0500

    changes done

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -247,6 +247,7 @@ exports.savedPosts = async (req, res) => {
     } else {
       user.saved.push(postId);
       await user.save();
+      await populate("saved");
       return res.status(200).json({
         success: true,
         message: "post saved",
